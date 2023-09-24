23675fed7bdfd36a939af08d33712dcb0e829c0c
aleksandrledenev <ledenevsanok@gmail.com>
2023-09-24 23:28:00 +0300

    Добавил файл в корень проекта + поправил ошибки в классе

--- src/main/java/ru/amm/ledenev/Main.java
+++ src/main/java/ru/amm/ledenev/Main.java
@@ -7,16 +7,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         try {
-            // Добавь файл test.txt в корень проекта и используй относительную ссылку (./test.txt)
-            Scanner scanner = new Scanner(new File("/Users/aleksandrledenev/Desktop/Лаба1/test.txt"));
+            Scanner scanner = new Scanner(new File("./test.txt"));
 
             // Следующее улучшение - попробуй прочитать студента, который записан в одну строку (помогут функции scanner.nextInt() и scanner.next())
             Student student = new Student();
-            student.FirstName = scanner.nextLine();
-            student.LastName = scanner.nextLine();
-            student.FatherName = scanner.nextLine();
-            student.Group = Integer.parseInt(scanner.nextLine());
-            student.Course = Integer.parseInt(scanner.nextLine());
+            student.firstName = scanner.nextLine();
+            student.lastname = scanner.nextLine();
+            student.fathername = scanner.nextLine();
+            student.group = Integer.parseInt(scanner.nextLine());
+            student.course = Integer.parseInt(scanner.nextLine());
 
             scanner.close();
         } catch (FileNotFoundException e) {
--- src/main/java/ru/amm/ledenev/Student.java
+++ src/main/java/ru/amm/ledenev/Student.java
@@ -2,11 +2,9 @@ package ru.amm.ledenev;
 
 // Имена полей с маленькой буквы (удобно переименовывать через Shift F6
 public class Student {
-    String FirstName;
-    String LastName;
-    String FatherName;
-    int Group;
-    int Course;
-// Лишние линии))
-
+    String firstName;
+    String lastname;
+    String fathername;
+    int group;
+    int course;
 }
