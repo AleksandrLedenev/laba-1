ab6a44eb0cab7b5ba78aeec1f8d0dcb4de57cab4
Sergey Volchenko <se.volchenko@ya.ru>
2023-09-24 23:13:45 +0300

    Добавил комментарии

--- src/main/java/ru/amm/ledenev/Main.java
+++ src/main/java/ru/amm/ledenev/Main.java
@@ -7,8 +7,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         try {
+            // Добавь файл test.txt в корень проекта и используй относительную ссылку (./test.txt)
             Scanner scanner = new Scanner(new File("/Users/aleksandrledenev/Desktop/Лаба1/test.txt"));
 
+            // Следующее улучшение - попробуй прочитать студента, который записан в одну строку (помогут функции scanner.nextInt() и scanner.next())
             Student student = new Student();
             student.FirstName = scanner.nextLine();
             student.LastName = scanner.nextLine();
--- src/main/java/ru/amm/ledenev/Student.java
+++ src/main/java/ru/amm/ledenev/Student.java
@@ -1,11 +1,12 @@
 package ru.amm.ledenev;
 
+// Имена полей с маленькой буквы (удобно переименовывать через Shift F6
 public class Student {
     String FirstName;
     String LastName;
     String FatherName;
     int Group;
     int Course;
-
+// Лишние линии))
 
 }
